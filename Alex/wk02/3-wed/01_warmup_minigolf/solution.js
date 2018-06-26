eb1d4441e7052c72c85d1cdee7b535af4cf676ad
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-27 09:23:08 +1000

    Add solution to initial MiniGolf totalling problem
    Add the initial solution to totalling an array of values to produce a
    score, before tackling the extension problem of determining scores
    against par.
    
    == References:
    - [Array.prototype.reduce - JavaScript | MDN]
      (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce)

--- Alex/wk02/3-wed/01_warmup_minigolf/solution.js
+++ Alex/wk02/3-wed/01_warmup_minigolf/solution.js
@@ -12,38 +12,83 @@
 // for each player and their combined total.
 
 // #### Bob
+var bobScores = [
 // - Hole 1 = 3
+  3,
 // - Hole 2 = 2
+  2,
 // - Hole 3 = 6
+  2,
 // - Hole 4 = 11
+  11,
 // - Hole 5 = 9
+  9,
 // - Hole 6 = 2
+  2,
 // - Hole 7 = 6
+  6,
 // - Hole 8 = 9
+  9,
 // - Hole 9 = 10
+  10
+];
 
 
 // #### Jimbo
+var jimboScores = [
 // - Hole 1 = 5
+  5,
 // - Hole 2 = 12
+  12,
 // - Hole 3 = 9
+  9,
 // - Hole 4 = 22
+  22,
 // - Hole 5 = 13
+  13,
 // - Hole 6 = 7
+  7,
 // - Hole 7 = 16
+  16,
 // - Hole 8 = 10
+  10,
 // - Hole 9 = 11
+  11
+];
 
 // #### Fish
+var fishScores = [
 // - Hole 1 = 2
+  2,
 // - Hole 2 = 2
+  2,
 // - Hole 3 = 4
+  4,
 // - Hole 4 = 5
+  5,
 // - Hole 5 = 4
+  4,
 // - Hole 6 = 3
+  3,
 // - Hole 7 = 6
+  6,
 // - Hole 8 = 4
+  4,
 // - Hole 9 = 1
+  1
+];
+
+var total = function(values) {
+  return values.reduce(function(sum, value) { return sum += value; }, 0);
+}
+
+// == References:
+// - [Array.prototype.reduce - JavaScript | MDN]
+//   (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce)
+
+console.log("  Bob scores: " + total(bobScores));
+console.log("Jimbo scores: " + total(jimboScores));
+console.log(" Fish scores: " + total(fishScores));
 
 // ## Extension
 // Now work out each golfers round compared to the course par.
