fa5d7b708d6649b22c0399790157bfd1ba91c0e1
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-27 15:41:30 +1000

    Add solution to Money Tree problem
    Add the solution to the Money Tree problem, which resolves the tree by
    determining the last line first and building the tree in reverse, until
    all rows / lines of the tree have been recorded. As each line is
    recorded the money symbol decreases in size by a half, down to a minimum
    of one `2n +1`, whilst simultaneously the edges increase by one each row
    heading up `n`. Therefore a loop the counts up (or down) increases the
    padding from zero on either side, whilst decreasing the repetition of
    the money symbol `$` in the middle.

--- Alex/wk02/3-wed/04_money_tree_js/solution.js
+++ Alex/wk02/3-wed/04_money_tree_js/solution.js
@@ -6,3 +6,40 @@
 
 
 // # Money Tree
+
+var moneyTree = function(size, symbol, padding) {
+  size = (typeof size !== 'undefined') ? size : 7;
+  symbol = (typeof symbol !== 'undefined') ? symbol : "$";
+  padding = (typeof padding !== 'undefined') ? padding : " ";
+
+  if (size === 1) {
+    return [symbol];
+  }
+
+  var lines = [];
+
+  // 2n +1
+
+  // last line first
+  // lines.push(symbol.repeat((2 * size) + 1))
+
+  // // second-last line first
+  // lines.push(padding + symbol.repeat((2 * (size - 1)) + 1) + padding)
+
+  // // third-last line first
+  // lines.push(padding.repeat(2) + symbol.repeat((2 * (size - 2)) + 1) + padding.repeat(2))
+
+  for (var i = 0; i <= size; i++) {
+    var line = padding.repeat(i) + symbol.repeat((2 * (size - i)) + 1) + padding.repeat(i);
+    lines.push(line);
+  }
+
+  return lines.reverse();
+};
+
+var logMoneyTree = function(size, symbol) {
+  var tree = moneyTree(size, symbol);
+  tree.forEach(function(line) { console.log(line); });
+}
+
+logMoneyTree(7);
