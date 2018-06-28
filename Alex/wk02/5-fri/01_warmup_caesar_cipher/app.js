52218ac91821e737d09cf333198c0d54fe818c0e
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-29 09:35:04 +1000

    Add initial solution to Caesar Cipher problem
    Add an initial solution to Caesar Cipher problem that relies on a
    `shiftAlphabet` function to build a cipher from a maybe given array of
    alphabet letters and shifts it by 3, when no number provided. The uses a
    `decrypt` method to find and replace letters within encrypted message
    with those from the cipher, with respect to the provided alphabet of
    letters.

--- Alex/wk02/5-fri/01_warmup_caesar_cipher/app.js
+++ Alex/wk02/5-fri/01_warmup_caesar_cipher/app.js
@@ -10,19 +10,45 @@
 
 // **FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV, WKH YDOLDQW QHYHU WDVWH RI GHDWK
 // EXW RQFH.**
+var secretMessage = 'FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV,' +
+                    ' WKH YDOLDQW QHYHU WDVWH RI GHDWK EXW RQFH.';
 
 // This is a form of cryptography known as the Caesar Cipher. It has a **shift
 // parameter** of **3**.
+// var shift = 3;
 
 // The alphabet is normally:
 
 // ABCDEFGHIJKLMNOPQRSTUVWXYZ
+// var aToZ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var alphabetString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var alphabet = alphabetString.split('');
 
 // The alphabet with the shift parameter of 3 is now as follows:
 
 // DEFGHIJKLMNOPQRSTUVWXYZABC
+// var shiftedAlphabet = 'DEFGHIJKLMNOPQRSTUVWXYZABC';
+var shiftedAlphabet = function(shift, alphabet) {
+  shift = (typeof shift !== 'undefined') ? shift : 3;
+  alphabet = (typeof alphabet !== 'undefined') ? alphabet : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
+  return alphabet.concat(alphabet.splice(0, shift));
+};
 
+var decrypt = function(encrypted, cipher, alphabet) {
+  alphabet = (typeof alphabet !== 'undefined') ? alphabet : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  cipher = (typeof cipher !== 'undefined') ? cipher : shiftedAlphabet(3, alphabet);
+
+  return encrypted.split('').map(function(char, index) {
+    if (char.match(/[a-z]/i)) {
+      return alphabet[cipher.indexOf(char)];
+    }
+    return char;
+  }).join('');
+}
+
+cipher = shiftedAlphabet(3, alphabet);
+console.log(decrypt(secretMessage, cipher));
 
 // ---
 // ### Extension:
