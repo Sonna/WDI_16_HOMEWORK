047ed6eaf75e1257fe278302c1ef73963894eb55
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-28 16:18:13 +1000

    Update Bank ATM App homework with functionality
    Update the Bank ATM application homework to have functionality according
    to the provided requirements, which state for given ATM elements on the
    page the amount given can either be deposited or withdrawn from the
    account when their respective button is press and update the account
    balance according, without going below `0` and updating the HTML
    elements to reflect thee changes.

--- Alex/wk02/4-thu/05_ga_bank_atm_app/app.js
+++ Alex/wk02/4-thu/05_ga_bank_atm_app/app.js
@@ -15,15 +15,80 @@
 // ### Specification:
 
 // * Keep track of the checking and savings balances somewhere
+var checking = { balance: 0 };
+var savings = { balance: 0 };
+
 // * Add functionality so that a user can deposit money into one of the bank
 //   accounts.
+var deposit = function(amount, account) {
+  account.balance += amount;
+  return account.balance;
+};
+
 // * Make sure you are updating the display and manipulating the HTML of the
 //   page so a user can see the change.
+var checkingEl = document.querySelector('#checking');
+var savingsEl = document.querySelector('#savings');
+
+var checkingBalanceEl = document.querySelector('#checking .balance');
+var savingsBalanceEl = document.querySelector('#savings .balance');
+
+var checkingAmountInputEl = document.querySelector('#checking input');
+var savingsAmountInputEl = document.querySelector('#savings input');
+
+var checkingDepositButtonEl = document.querySelector('#checking button.deposit');
+var savingsDepositButtonEl = document.querySelector('#savings button.deposit');
+var checkingWithdrawButtonEl = document.querySelector('#checking button.withdraw');
+var savingsWithdrawButtonEl = document.querySelector('#savings button.withdraw');
+
+checkingBalanceEl.textContent = checking.balance;
+savingsBalanceEl.textContent = savings.balance;
+
 // * Add functionality so that a user can withdraw money from one of the bank
 //   accounts.
+var withdraw = function(amount, account) {
+  if (account.balance - amount >= 0) {
+    account.balance -= amount;
+  }
+  return account.balance;
+};
+
 // * Make sure you are updating the display and manipulating the HTML of the
 //   page so a user can see the change.
 // * Make sure the balance in an account can't go negative. If a user tries to
 //   withdraw more money than exists in the account, ignore the transaction.
 // * When the balance of the bank account is $0 the background of that bank
 //   account should be red. It should be gray when there is money in the account
+var updateAccountBackground = function(account, el) {
+  if (account.balance === 0) {
+    el.style.backgroundColor = 'red';
+  } else {
+    el.style.backgroundColor = 'gray';
+  }
+}
+updateAccountBackground(checking, checkingEl);
+updateAccountBackground(savings, savingsEl);
+
+checkingDepositButtonEl.addEventListener('click', function() {
+  var amount = Number(checkingAmountInputEl.value);
+  checkingBalanceEl.textContent = deposit(amount, checking);
+  updateAccountBackground(checking, checkingEl);
+});
+
+savingsDepositButtonEl.addEventListener('click', function() {
+  var amount = Number(savingsAmountInputEl.value);
+  savingsBalanceEl.textContent = deposit(amount, savings);
+  updateAccountBackground(savings, savingsEl);
+});
+
+checkingWithdrawButtonEl.addEventListener('click', function() {
+  var amount = Number(checkingAmountInputEl.value);
+  checkingBalanceEl.textContent = withdraw(amount, checking);
+  updateAccountBackground(checking, checkingEl);
+});
+
+savingsWithdrawButtonEl.addEventListener('click', function() {
+  var amount = Number(savingsAmountInputEl.value);
+  savingsBalanceEl.textContent = withdraw(amount, savings);
+  updateAccountBackground(savings, savingsEl);
+});
