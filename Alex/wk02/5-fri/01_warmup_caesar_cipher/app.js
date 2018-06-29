99c86ac280721b4e0e1f49771d1fcbf74117a374
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-29 10:04:56 +1000

    Add Caesar Cipher class with extension functions
    Add a Caesar Cipher class with a collection of static variables and
    functions that implement the building of a Cipher, whilst encoding and
    decrypting text that use them. Then demonstrate that a cipher can be
    built and used to encrypt and decrypt text.

--- Alex/wk02/5-fri/01_warmup_caesar_cipher/app.js
+++ Alex/wk02/5-fri/01_warmup_caesar_cipher/app.js
@@ -22,7 +22,7 @@ var secretMessage = 'FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV,' +
 // ABCDEFGHIJKLMNOPQRSTUVWXYZ
 // var aToZ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var alphabetString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var alphabet = alphabetString.split('');
+// var alphabet = alphabetString.split('');
 
 // The alphabet with the shift parameter of 3 is now as follows:
 
@@ -47,7 +47,7 @@ var decrypt = function(encrypted, cipher, alphabet) {
   }).join('');
 }
 
-cipher = shiftedAlphabet(3, alphabet);
+cipher = shiftedAlphabet(3, alphabetString.split(''));
 console.log(decrypt(secretMessage, cipher));
 
 // ---
@@ -57,9 +57,55 @@ console.log(decrypt(secretMessage, cipher));
 // ```
 // CaesarCipher.encode('TWO SYMBOLS KISSING EACH OTHER')
 // ```
+function CaesarCipher() {}
+CaesarCipher.defaultShiftNum = 3;
+CaesarCipher.defaultAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+CaesarCipher.build = function(shift, alphabet) {
+  shift = (typeof shift !== 'undefined') ? shift : this.defaultShiftNum;
+  alphabet = (typeof alphabet !== 'undefined') ? alphabet : this.defaultAlphabet;
+
+  return alphabet.slice(shift, alphabet.length).concat(alphabet.slice(0, shift));
+};
+
+CaesarCipher.decrypt = function(encrypted, cipher, alphabet) {
+  var self = this;
+  alphabet = (typeof alphabet !== 'undefined') ? alphabet : this.defaultAlphabet;
+  cipher = (typeof cipher !== 'undefined') ? cipher : this.build(self.defaultShiftNum, alphabet);
+
+  return encrypted.split('').map(function(char, index) {
+    if (char.match(/[a-z]/i)) {
+      return alphabet[cipher.indexOf(char)];
+    }
+    return char;
+  }).join('');
+}
+
+// CaesarCipher.prototype.encode = function(plain) {
+CaesarCipher.encode = function(plain, cipher, alphabet) {
+  // return this.decrypt(plain, alphabet, cipher);
+  alphabet = (typeof alphabet !== 'undefined') ? alphabet : this.defaultAlphabet;
+  cipher = (typeof cipher !== 'undefined') ? cipher : this.build(3, alphabet);
+
+  return plain.split('').map(function(char, index) {
+    if (char.match(/[a-z]/i)) {
+      return cipher[alphabet.indexOf(char)];
+    }
+    return char;
+  }).join('');
+};
+
+var newSecretMessage = CaesarCipher.encode('TWO SYMBOLS KISSING EACH OTHER');
+console.log(newSecretMessage);
+console.log(CaesarCipher.decrypt(newSecretMessage));
 
 // ### Extension 2:
 
 // Write the program to encode it with any shift parameter.
 
 // Send each other secret messages.
+
+var cipher = CaesarCipher.build(4);
+var newSecretMessage = CaesarCipher.encode('TWO SYMBOLS KISSING EACH OTHER', cipher);
+console.log(newSecretMessage);
+console.log(CaesarCipher.decrypt(newSecretMessage, cipher));
