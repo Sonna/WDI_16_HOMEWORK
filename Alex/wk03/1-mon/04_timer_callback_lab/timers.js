d961368c9238ca13a92e9990b46f218d32aec178
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-02 16:28:31 +1000

    Add `timer` object to re-implement `timers.js`
    Add the `timer` object to reimplement the behaviour within `timers.js`,
    but keep it separate within its own `timer.js` script in order to keep
    the original as is. This reimplements the Stop Watch behaviours for each
    button, but within the `var timer` variable (for better or worse) as
    requested by the extension activity.

--- Alex/wk03/1-mon/04_timer_callback_lab/timers.js
+++ Alex/wk03/1-mon/04_timer_callback_lab/timers.js
@@ -86,6 +86,30 @@ resetBtn.addEventListener('click', function() {
 
 // var timer = {
 //   // All your code goes in here...
+//   seconds: 0,
+//   timerId: 0,
+//   originalStopWatchText: "Stop Watch",
+
+//   updateTime: function() {
+//     timer.seconds += 1;
+//     document.querySelector('h1#timer').textContent = timer.seconds;
+//   },
+
+//   startBtn: document.querySelector('#start').addEventListener('click', function() {
+//     if (!timer.timerId) { // technically `0` or `undefined` not `false`
+//       timer.timerId = setInterval(timer.updateTime, 1000);
+//     }
+//   }),
+
+//   pauseBtn: document.querySelector('#pause').addEventListener('click', function() {
+//     timer.timerId = clearInterval(timer.timerId);
+//   }),
+
+//   resetBtn: document.querySelector('#reset').addEventListener('click', function() {
+//     timer.timerId = clearInterval(timer.timerId);
+//     timer.seconds = 0;
+//     document.querySelector('h1#timer').textContent = timer.originalStopWatchText;
+//   })
 // }
 // ```
 
