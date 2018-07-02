75026794ab12e4d35e3f251f3efb33ca4f5b303f
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-02 16:43:28 +1000

    Refactor Event bindings on Stop Watch in function
    Refactor the binding of Events within the `timer` variable into a
    constructor like function that can be called after the window has loaded
    and the script has been loaded, rather adding events within an Object
    property.

--- Alex/wk03/1-mon/04_timer_callback_lab/timer.js
+++ Alex/wk03/1-mon/04_timer_callback_lab/timer.js
@@ -9,19 +9,26 @@ var timer = {
     document.querySelector('h1#timer').textContent = timer.seconds;
   },
 
-  startBtn: document.querySelector('#start').addEventListener('click', function() {
+  startBtn: function() {
     if (!timer.timerId) { // technically `0` or `undefined` not `false`
       timer.timerId = setInterval(timer.updateTime, 1000);
     }
-  }),
+  },
 
-  pauseBtn: document.querySelector('#pause').addEventListener('click', function() {
+  pauseBtn: function() {
     timer.timerId = clearInterval(timer.timerId);
-  }),
+  },
 
-  resetBtn: document.querySelector('#reset').addEventListener('click', function() {
+  resetBtn: function() {
     timer.timerId = clearInterval(timer.timerId);
     timer.seconds = 0;
     document.querySelector('h1#timer').textContent = timer.originalStopWatchText;
-  })
+  },
+
+  initialize: function() {
+    document.querySelector('#start').addEventListener('click', timer.startBtn);
+    document.querySelector('#pause').addEventListener('click', timer.pauseBtn);
+    document.querySelector('#reset').addEventListener('click', timer.resetBtn);
+  }
 }
+timer.initialize();
