c70b2b118e4902032f56fa3825040bf2e41f9a90
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-02 16:53:25 +1000

    Refactor query Selectors into `timer` properties
    Refactor the common query selectors into their own object properties
    within the `timer` object, to help clean up code, call
    `document.querySelector` when setting attributes, and also open up the
    `timer` object for rebinding of HTML elements before `initialize` method
    is called to bind event listeners.

--- Alex/wk03/1-mon/04_timer_callback_lab/timer.js
+++ Alex/wk03/1-mon/04_timer_callback_lab/timer.js
@@ -4,31 +4,36 @@ var timer = {
   timerId: 0,
   originalStopWatchText: "Stop Watch",
 
+  heading: document.querySelector('h1#timer'),
+  startBtn: document.querySelector('#start'),
+  pauseBtn: document.querySelector('#pause'),
+  resetBtn: document.querySelector('#reset'),
+
   updateTime: function() {
     this.seconds += 1;
-    document.querySelector('h1#timer').textContent = this.seconds;
+    this.heading.textContent = this.seconds;
   },
 
-  startBtn: function() {
+  start: function() {
     if (!this.timerId) { // technically `0` or `undefined` not `false`
       this.timerId = setInterval(this.updateTime.bind(this), 1000);
     }
   },
 
-  pauseBtn: function() {
+  pause: function() {
     this.timerId = clearInterval(this.timerId);
   },
 
-  resetBtn: function() {
+  reset: function() {
     this.timerId = clearInterval(this.timerId);
     this.seconds = 0;
-    document.querySelector('h1#timer').textContent = this.originalStopWatchText;
+    this.heading.textContent = this.originalStopWatchText;
   },
 
   initialize: function() {
-    document.querySelector('#start').addEventListener('click', this.startBtn.bind(this));
-    document.querySelector('#pause').addEventListener('click', this.pauseBtn.bind(this));
-    document.querySelector('#reset').addEventListener('click', this.resetBtn.bind(this));
+    this.startBtn.addEventListener('click', this.start.bind(this));
+    this.pauseBtn.addEventListener('click', this.pause.bind(this));
+    this.resetBtn.addEventListener('click', this.reset.bind(this));
   }
 }
 timer.initialize();
