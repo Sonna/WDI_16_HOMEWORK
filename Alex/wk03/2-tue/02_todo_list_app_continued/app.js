1b3e208b9ca2a6bc7e307727cea22fd973f8a7c5
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-03 12:17:54 +1000

    Add copy of instructor's code to ToDo app
    Add a copy of the instructor's code to the To Do application to add new
    Items via a `button` click event, as part of the Code Along task for
    Week 03 Day 2 part 2.
    
    == Notes:
    - The `keydown` event code is my own code, written before the
      Instructor's version and before adding the `form`

--- Alex/wk03/2-tue/02_todo_list_app_continued/app.js
+++ Alex/wk03/2-tue/02_todo_list_app_continued/app.js
@@ -4,9 +4,42 @@ var footer = document.querySelector('footer');
 var completedCount = document.querySelector('.completed-count');
 var todoItems = document.querySelectorAll('li'); // find all todo items
 var resetBtn = document.querySelector('button.reset');
+var newTodoInput = document.querySelector('.new-todo-input');
+var addTodoBtn = document.querySelector('form button');
+var todoList = document.querySelector('.todo-list');
 
-var updateCompleteCount = function() {
+addTodoBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  // console.log('adding todo....');
+
+  // get value from input
+  var newTodoValue = newTodoInput.value;
+  if (newTodoValue === '') { return; }
+
+  // create new `li`
+  var newLi = document.createElement('li');
+  // console.log(newLi);
+  // set `li` text content to value
+  newLi.textContent = newTodoValue;
+  // console.log(newLi);
+
+  // add todo-item class to newLi
+  newLi.classList.add('todo-item');
+  // prepare it for the real world
+  newLi.addEventListener('click', markComplete);
+
+  // append it to `ul`
+  todoList.appendChild(newLi);
+
+  // clear text (from input field)
+  // set input value to empty string
+  newTodoInput.value = '';
+  updateCompleteCount();
+});
+
+function updateCompleteCount() {
   completedCount.textContent = document.querySelectorAll('.completed').length;
+  todoItems = document.querySelectorAll('li'); // re-evaluate
 
   if (Number(completedCount.textContent) === todoItems.length) {
     footer.classList.remove('hidden');
@@ -15,7 +48,7 @@ var updateCompleteCount = function() {
   }
 }
 
-var markComplete = function(event) {
+function markComplete(event) {
   event.target.classList.toggle('completed');
   updateCompleteCount();
 };
@@ -25,9 +58,29 @@ todoItems.forEach(function(item) {
 });
 
 var resetTodos = function() {
+  todoItems = document.querySelectorAll('li'); // re-evaluate
   todoItems.forEach(function(item) {
     item.classList.remove('completed');
   });
+  footer.classList.add('hidden'); // hide after reset
 }
 
 resetBtn.addEventListener('click', resetTodos);
+
+var addNewTodoItem = function(value) {
+  if (value === '') { return; }
+  var newTodoItem = document.createElement('li');
+  newTodoItem.classList.add('todo-item');
+  newTodoItem.textContent = value;
+  newTodoItem.addEventListener('click', markComplete);
+  todoList.appendChild(newTodoItem);
+  updateCompleteCount();
+};
+
+newTodoInput.addEventListener('keydown', function(event) {
+  // console.log(event);
+  if (event.code === "Enter") {
+    addNewTodoItem(event.target.value);
+    event.target.value = ''; // Clear Input
+  }
+})
