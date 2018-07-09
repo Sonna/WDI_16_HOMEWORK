6814a088bd3bf0cf76cb145ae55d3b174196020a
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-09 16:18:06 +1000

    Update Code Along with in progress work
    Update the Code Along with the work in progress, since I had to leave
    the room to take part in an interview off site.

--- Alex/wk04/1-mon/04_palindrome_ruby/code_along.rb
+++ Alex/wk04/1-mon/04_palindrome_ruby/code_along.rb
@@ -1,4 +1,4 @@
-require "pry"
+# require "pry"
 
 # "oxo"
 # "hannah"
@@ -6,8 +6,36 @@ require "pry"
 # "a toyota"
 # "evan"
 
-exhibit1 = "oxo"
+# exhibit1 = "oxo"
+# exhibit1 = "hannah"
+# exhibit1 = "evan"
+exhibit1 = "a toyota"
+exhibit1.reverse() # => "atoyot a"
 
-binding.pry
+'a toyota'.split(' ').join('') # => "atoyota"
+exhibit1.gsub('a', 'n') # => "n toyotn"
+exhibit1.gsub('o', 'x') # => "a txyxta"
+exhibit1.gsub(/[^a-z]/, '') # => "atoyota"
+exhibit1.gsub(/[\w]*=/, '') # => "a toyota"
+
+"apple" == "orange" # => false
+'6' == 6 # => false
+'6'.to_i == 6 # => true
+
+if exhibit1 == exhibit1.reverse()
+  puts("yes")
+else
+  puts("no")
+end
+
+# left class for interview
+
+# binding.pry
 
 puts('just me')
+
+# regex - regular expressions
+# /[\w]*=/
+
+# >> no
+# >> just me
