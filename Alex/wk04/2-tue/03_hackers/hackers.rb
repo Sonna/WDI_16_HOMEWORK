1f5f2eb4307946c14e0ee67293b6caf4af71e19f
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-10 12:41:22 +1000

    Add problem & solution to Week 04 Day 2 Part 3
    Add the problem statement and solution to the `hackers.rb` script for
    Week 04 Day 2 part 3 task, whilst ignoring the directories / files it
    creates.

---
+++ Alex/wk04/2-tue/03_hackers/hackers.rb
@@ -0,0 +1,29 @@
+=begin
+given a list of names
+Joe Black
+Mary Jane
+Bob Hawke
+
+hackers
+  |
+  |--joe_black
+  |
+  |--mary_jane
+  |
+  |--bob_hawke
+
+=end
+
+require "fileutils"
+
+names = [
+  "Joe Black",
+  "Mary Jane",
+  "Bob Hawke"
+]
+
+directories = names.map { |name| name.downcase.gsub(/[^A-Za-z]/, '_')  }
+
+FileUtils.mkdir(directories)
+
+directories.each { |directory| FileUtils.touch("#{directory}/README.md") }
