aea1a8cf4a66725b25c221f2ea355bc990f18557
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-10 14:32:32 +1000

    Update Week 04 Day 2 code along work
    Update the Week four day two part three "hackers" directory work with
    notes copied from the instructor with their solution to the make
    directories with README files problem.
    
    == Notes:
    - Fix Tree directory representation within the comments

--- Alex/wk04/2-tue/03_hackers/hackers.rb
+++ Alex/wk04/2-tue/03_hackers/hackers.rb
@@ -7,22 +7,40 @@ Bob Hawke
 hackers
   |
   |--joe_black
+  |     |-README.md
   |
   |--mary_jane
+  |     |-README.md
   |
   |--bob_hawke
+        |-README.md
 
 =end
 
 require "fileutils"
 
-names = [
+hackers = [
   "Joe Black",
   "Mary Jane",
   "Bob Hawke"
 ]
 
-directories = names.map { |name| name.downcase.gsub(/[^a-z]/, '_')  }
+# that do end thing?? - ruby blocks
+hackers.each do |hacker|
+  # secret = 42 # scoped local to the block
+  dir_name = hacker.downcase.gsub(" ", "")
+  FileUtils.mkdir(dir_name)
+  FileUtils.touch("#{dir_name}/README.md")
+end
+
+# loop for each hackers
+
+# make a dir with the hackers name
+# cd into dir
+# touch a readme.md file
+# cd ..
+
+directories = hackers.map { |hacker| hacker.downcase.gsub(/[^a-z]/, '_')  }
 
 FileUtils.mkdir(directories)
 
