69a02f93380e6a72fa77af659f15ea5cc685bafe
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-11 10:26:46 +1000

    Add Prefect Squares solutions based off discussion
    Add some additional Prefect Squares solutions based off the initial
    discussion within the class, since I forgot to use `select` and use
    `uniq` first before building a solution.

--- Alex/wk04/3-wed/01_warmup_ruby_squares/get_squares.rb
+++ Alex/wk04/3-wed/01_warmup_ruby_squares/get_squares.rb
@@ -18,12 +18,15 @@
 # numbers. This method will return a new array of numbers that qualify as
 # perfect squares from the argument array.
 
-def get_squares1(numbers)
+def get_squares_v1(numbers)
   numbers.each_with_object([]) do |number, result|
     # result.push(number) if Math.sqrt(number) % 1 == 0
     result.push(number) if Math.sqrt(number).modulo(1).zero?
   end.uniq.sort
 end
+
+get_squares_v1([4, 1, 16, 1, 10, 35, 22]) # => [1, 4, 16]
+
 
 # Or a `Set` could be used to only keep unique values
 require "set"
@@ -44,6 +47,14 @@ end
 get_squares([4, 1, 16, 1, 10, 35, 22]) # => [1, 4, 16]
 # ```
 
+def get_squares_v3(numbers)
+  # numbers.find_all { |n| Math.sqrt(n).modulo(1).zero? }.uniq.sort
+  # numbers.select { |n| Math.sqrt(n).modulo(1).zero? }.uniq.sort
+  numbers.uniq.select { |n| Math.sqrt(n).modulo(1).zero? }.sort
+end
+get_squares_v3([4, 1, 16, 1, 10, 35, 22]) # => [1, 4, 16]
+
+
 # ### Done?
 # Solve it with a JavaScript function!
 
@@ -58,3 +69,4 @@ get_squares([4, 1, 16, 1, 10, 35, 22]) # => [1, 4, 16]
 # ```
 
 # What's that all about?!?!?!
+# ~>  from -:54:in `<main>'
