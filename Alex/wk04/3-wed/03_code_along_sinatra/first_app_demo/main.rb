7489fbbe4458020f8cbe72f53c4adc7beda948f2
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-11 12:36:46 +1000

    Add Week 04 Day 3 Part 3 work
    Add the Week four day three part three code along work that builds an
    example of a Sinatra Web / Application server that responds simply to a
    few requests sent to it.

---
+++ Alex/wk04/3-wed/03_code_along_sinatra/first_app_demo/main.rb
@@ -0,0 +1,51 @@
+require "sinatra"
+require "sinatra/reloader"
+
+# http request - query string - anything that comes after a question mark
+# query string - key value pair
+# url https://dt.com:4567/about?name=dt
+# params is magical variable that contains your query string pair
+# different methods - get
+
+get "/" do
+  return "welcome to my home page"
+end
+
+get "/about" do
+  # return "its me. alex"
+  # return "its me. alex #{Time.now}"
+  return "<h1>its me. alex</h1>"
+end
+
+# localhost:4567/hello?name=slackers
+get "/hello" do
+  # name = "dt"
+  # params['name']
+
+  # return "<h2>hello #{ name }</h2>"
+  # return "<h2>hello #{ params['name'] }</h2>"
+  return "<h2>Yo #{ params['name'] }</h2>"
+end
+
+# if the client request this path - and provide query string num1 & num2
+# localhost:4567/crappy_calc?num1=5&num2=6
+# respond with the sum of the numbers
+get "/crappy_calc" do
+  # your turn
+  # return params['num1'] + params['num2']
+  return (params['num1'].to_i + params['num2'].to_i).to_s
+end
+
+# get "/" do
+#   <<~HTML
+#     <!DOCTYPE html>
+#     <html>
+#     <head>
+#       <title>Hello from Sinatra</title>
+#     </head>
+#     <body>
+#       <p>Yo!</p>
+#     </body>
+#     </html>
+#   HTML
+# end
