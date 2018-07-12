082cc22e088feb722bbe4c8e6194e93c9770a5fc
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-12 11:58:06 +1000

    Add all demonstration notes within Stocks app
    Add all demonstration notes from when presenting to the class and
    discuss various ideas; like "crab claws", embedded Ruby, calling
    templates etc.

--- Alex/wk04/3-wed/04_stocks/main.rb
+++ Alex/wk04/3-wed/04_stocks/main.rb
@@ -2,9 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 require "stock_quote"
 
-# localhost:4567/?stock=MSFT
-get "/" do
-  stock = params['stock']
-  quote = StockQuote::Stock.quote(stock)
-  erb :index, locals: { quote: quote, stock: stock }
+# localhost:4567/?stock=appl
+get "/" do #        v
+  # params = { 'stock' => 'MSFT' } # Sinatra provides this `params`
+  @stock = params['stock']
+  @quote = StockQuote::Stock.quote(@stock) # || nil
+  if @quote
+    return erb(:index)
+  else
+    return erb(:bananas) #, locals: { quote: quote, stock: stock }
+  end
+end
+
+# get "/index"
+get "/stock_request" do
+  return erb(:bananas)
 end
