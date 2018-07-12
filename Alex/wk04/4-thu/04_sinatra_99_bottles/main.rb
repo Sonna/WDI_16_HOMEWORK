8cd87ecf13fc396316af491a3e4c761d05c875db
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-12 15:38:26 +1000

    Add solution to "99 Bottles of Beer" sinatra app
    Add a solution to the "99 Bottles of Beer" Sinatra application, which
    relies on named parameters within the route to determine the route taken
    and contents of the page, providing a page with the number of bottles on
    the wall with a link to take the next one down (decrease it by one).
    
    Also, adds a route for `0` bottles to explain no bottles on the wall and
    link to start again.

--- Alex/wk04/4-thu/04_sinatra_99_bottles/main.rb
+++ Alex/wk04/4-thu/04_sinatra_99_bottles/main.rb
@@ -1,9 +1,34 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+# ## Project Specs
+
+# create the 99 bottles of beer song in the form of a sinatra web application.
+
+# - The home page `/` should list:
+#   - "99 Bottles of beer on the wall"
+#   - add a link to take one down
+# - The page `/:num_bottles` should show:
+#   - `:num_bottles` bottles of beer on the wall"
+#   - where `:num_bottles` can be any positive integer.
+#   - add a link to take one down
+# - The page `/0` should show:
+#   - "No Bottles of beer on the wall"
+#   - do not show a link to take one down
+#   - show a link to start over
+
 BEER_BOTTLES = 99
 
 get '/' do
-  @num_of_bottles = BEER_BOTTLES - params['take_down'].to_i
+  redirect "/#{BEER_BOTTLES}"
+end
+
+get '/0' do
+  erb :zero
+end
+
+get '/:num_bottles' do
+  @num_of_bottles = params[:num_bottles].to_i
   erb :index
 end
