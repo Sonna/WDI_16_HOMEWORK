327ed3272b49abcb7a59780d363431ff668c35fb
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-13 17:02:18 +1000

    Add initial layout of Menu CLI interface
    Add the initial layout for calling different methods from the Shelter
    class and provide these methods stubs that can be called, but might do
    anything yet.
    
    Also, Update Animal to return itself as a string when displayed to the
    screen.

--- Alex/wk04/5-fri/04_homework_happi_tials/happitials.rb
+++ Alex/wk04/5-fri/04_homework_happi_tials/happitials.rb
@@ -32,9 +32,13 @@ class Animal
     @species = species
     @toys = toys
   end
+
+  def to_s
+    "#{name} #{age} #{gender} #{species} #{toys.join(', ')}"
+  end
 end
 
-Animal.new # => #<Animal:0x00007f8b4f05d8e0 @name="", @age=0, @gender="", @species="", @toys=[]>
+Animal.new # => #<Animal:0x00007fadc693f668 @name="", @age=0, @gender="", @species="", @toys=[]>
 
 # - Client:
 #   - A client should have a name.
@@ -54,7 +58,7 @@ class Client
   end
 end
 
-Client.new # => #<Client:0x00007f8b4f03f480 @name="", @age=0, @children=[], @pets=[]>
+Client.new # => #<Client:0x00007fadc693e010 @name="", @age=0, @num_children=0, @pets=[]>
 
 # - Shelter:
 #   - The shelter should display all the clients.
@@ -69,7 +73,7 @@ class Shelter
   end
 end
 
-Shelter.new # => #<Shelter:0x00007f8b4f03d540 @clients=[], @animals=[]>
+Shelter.new # => #<Shelter:0x00007fadc6919670 @clients=[], @animals=[]>
 
 # - Relationships:
 #   - A client should be able to adopt an animal.
@@ -88,21 +92,16 @@ end
 
 # - Can create animals and clients
 
-robin = Client.new('Christopher Robin', 9) # => #<Client:0x00007f8b4f03c208 @name="Christopher Robin", @age=9, @children=[], @pets=[]>
+robin = Client.new('Christopher Robin', 9) # => #<Client:0x00007fadc7053378 @name="Christopher Robin", @age=9, @num_children=0, @pets=[]>
 aa_milne = Client.new('Alan Alexander Milne', 30, 1)
-# => #<Client:0x00007f8b500b5558
+# => #<Client:0x00007fadc78579c0
 #     @age=30,
-#     @children=
-#      [#<Client:0x00007f8b4f03c208
-#        @age=9,
-#        @children=[],
-#        @name="Christopher Robin",
-#        @pets=[]>],
 #     @name="Alan Alexander Milne",
+#     @num_children=1,
 #     @pets=[]>
 
 pooh = Animal.new('winnie', 2, 'male', 'stuffed bear', 'honey', 'red shirt')
-# => #<Animal:0x00007f8b50073360
+# => #<Animal:0x00007fadc785fdc8
 #     @age=2,
 #     @gender="male",
 #     @name="winnie",
@@ -110,7 +109,7 @@ pooh = Animal.new('winnie', 2, 'male', 'stuffed bear', 'honey', 'red shirt')
 #     @toys=["honey", "red shirt"]>
 
 tigger = Animal.new('tigger', 4, 'male', 'stuffed tiger', 'A bouncing ball', 'A jumping jack', 'A jumping Jill', 'A rocket ship', 'A couple dozen banana splits', 'A Pogo stick', 'A trampoline', 'A flying machine', 'A banana split', 'A Pogo stick ', 'Book on "Tigers bounce best"')
-# => #<Animal:0x00007f8b500a7318
+# => #<Animal:0x00007fadc783e808
 #     @age=4,
 #     @gender="male",
 #     @name="tigger",
@@ -128,14 +127,14 @@ tigger = Animal.new('tigger', 4, 'male', 'stuffed tiger', 'A bouncing ball', 'A 
 #       "A Pogo stick ",
 #       "Book on \"Tigers bounce best\""]>
 
-kanga = Animal.new('kanga', 1, 'female', 'kangaroo') # => #<Animal:0x00007f8b5006d190 @name="kanga", @age=1, @gender="female", @species="kangaroo", @toys=[]>
-roo = Animal.new('roo', 1, 'male', 'kangaroo') # => #<Animal:0x00007f8b5006c740 @name="roo", @age=1, @gender="male", @species="kangaroo", @toys=[]>
-eyore = Animal.new('eyore', 1, 'male', 'donkey') # => #<Animal:0x00007f8b5006b4a8 @name="eyore", @age=1, @gender="male", @species="donkey", @toys=[]>
-piglet = Animal.new('piglet', 1, 'male', 'pig') # => #<Animal:0x00007f8b5006a490 @name="piglet", @age=1, @gender="male", @species="pig", @toys=[]>
-rabbit = Animal.new('rabbit', 1, 'female', 'rabbit', 'carrots') # => #<Animal:0x00007f8b50068230 @name="rabbit", @age=1, @gender="female", @species="rabbit", @toys=["carrots"]>
-gopher = Animal.new('gopher', 1, 'male', 'gopher') # => #<Animal:0x00007f8b50065e40 @name="gopher", @age=1, @gender="male", @species="gopher", @toys=[]>
-owl = Animal.new('owl', 1, 'male', 'owl') # => #<Animal:0x00007f8b50065210 @name="owl", @age=1, @gender="male", @species="owl", @toys=[]>
-lumpy = Animal.new('lumpy', 1, 'male', 'heffalump') # => #<Animal:0x00007f8b500646d0 @name="lumpy", @age=1, @gender="male", @species="heffalump", @toys=[]>
+kanga = Animal.new('kanga', 1, 'female', 'kangaroo') # => #<Animal:0x00007fadc7847f70 @name="kanga", @age=1, @gender="female", @species="kangaroo", @toys=[]>
+roo = Animal.new('roo', 1, 'male', 'kangaroo') # => #<Animal:0x00007fadc78458d8 @name="roo", @age=1, @gender="male", @species="kangaroo", @toys=[]>
+eyore = Animal.new('eyore', 1, 'male', 'donkey') # => #<Animal:0x00007fadc7844d70 @name="eyore", @age=1, @gender="male", @species="donkey", @toys=[]>
+piglet = Animal.new('piglet', 1, 'male', 'pig') # => #<Animal:0x00007fadc7844208 @name="piglet", @age=1, @gender="male", @species="pig", @toys=[]>
+rabbit = Animal.new('rabbit', 1, 'female', 'rabbit', 'carrots') # => #<Animal:0x00007fadc782b370 @name="rabbit", @age=1, @gender="female", @species="rabbit", @toys=["carrots"]>
+gopher = Animal.new('gopher', 1, 'male', 'gopher') # => #<Animal:0x00007fadc782a768 @name="gopher", @age=1, @gender="male", @species="gopher", @toys=[]>
+owl = Animal.new('owl', 1, 'male', 'owl') # => #<Animal:0x00007fadc7829cc8 @name="owl", @age=1, @gender="male", @species="owl", @toys=[]>
+lumpy = Animal.new('lumpy', 1, 'male', 'heffalump') # => #<Animal:0x00007fadc7828e18 @name="lumpy", @age=1, @gender="male", @species="heffalump", @toys=[]>
 
 # ##### Phase 2
 
@@ -144,6 +143,16 @@ hundred_acre_woods = Shelter.new(
   [robin, aa_milne],
   [pooh, tigger, kanga, roo, eyore, piglet, rabbit, gopher, owl, lumpy]
 )
+
+class Shelter
+  def add_client(client)
+    @clients.push(client)
+  end
+
+  def add_animal(animal)
+    @animals.push(animal)
+  end
+end
 
 # ##### Phase 3
 
@@ -163,7 +172,7 @@ loop do
     name = gets.chomp
 
     print "Its age?"
-    age = gets.chomp
+    age = gets.chomp.to_i
 
     print "Its gender?"
     gender = gets.chomp
@@ -174,19 +183,36 @@ loop do
     toys = []
     loop do
       print "Give/add a Toy to the Animal? (leave blank to stop adding toys)"
-      break if (toy = gets.chomp)
+      break if (toy = gets.chomp).empty?
       toys.push(toy)
     end
 
   when "2", "client"
+    print "A name for the new Client?"
+    name = gets.chomp
+
+    print "Their age?"
+    age = gets.chomp.to_i
+
+    print "Their gender?"
+    gender = gets.chomp
+
+    print "Number of children?"
+    species = gets.chomp.to_i
+
+    pets = []
+    loop do
+      print "Name of a pet? (leave blank to stop adding pets)"
+      break if (pet = gets.chomp).empty?
+      pets.push(pet)
+    end
   else
     puts "Unkown choice"
   end
 
   print "Add another Animal or Client? (y/n)"
-  break if (gets.chomp == "y")
+  break unless (gets.chomp == "y")
 end
-
 
 # ##### Phase 4
 
@@ -200,3 +226,58 @@ end
 
 # - After selecting from the menu the task the user is prompted through the
 #   entire process
+
+class Shelter
+  def display_all_animals
+    animals
+  end
+
+  def display_all_clients
+  end
+
+  def create_an_animal
+  end
+
+  def create_an_client
+  end
+
+  def facilitate_client_adopts_an_animal
+  end
+
+  def facilitate_client_puts_an_animal_up_for_adoption
+  end
+end
+
+# require 'pry'
+# binding.pry
+shelter = hundred_acre_woods
+# shelter = Shelter.new(
+#   [robin, aa_milne],
+#   [pooh, tigger, kanga, roo, eyore, piglet, rabbit, gopher, owl, lumpy]
+# )
+
+loop do
+  puts <<~MENU
+    1) display all animals
+    2) display all clients
+    3) create an animal
+    4) create an client
+    5) facilitate client adopts an animal
+    6) facilitate client puts an animal up for adoption
+    0) quit
+  MENU
+  choice = gets.chomp
+
+  puts case choice.to_i
+    when 0 then break
+    when 1 then shelter.display_all_animals
+    when 2 then shelter.display_all_clients
+    when 3 then shelter.create_an_animal
+    when 4 then shelter.create_an_client
+    when 5 then shelter.facilitate_client_adopts_an_animal
+    when 6 then shelter.facilitate_client_puts_an_animal_up_for_adoption
+    else
+      "Unknown input, please try again"
+    end
+  puts "\n" # newline space between ouput & menu
+end
