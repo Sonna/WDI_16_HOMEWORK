734051b36519d15fd904ebe4505b2f3b2b48062f
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-13 12:56:29 +1000

    Add Ruby syntax sugar for getter & setter methods

--- Alex/wk04/5-fri/02_classes_oop/bank.rb
+++ Alex/wk04/5-fri/02_classes_oop/bank.rb
@@ -17,11 +17,18 @@ class Account
     return @balance
   end
 
-  def set_name(name = 'savings')
-    @name = name
+  # setter
+  # def set_name(name = 'savings')
+  # def name=(name)
+  #   @name = name
+  # end
+  def name=(value)
+    @name = value
   end
 
-  def get_name
+  # getter
+  # def get_name
+  def name
     # 'savings'
     @name
   end
@@ -37,43 +44,50 @@ end
 
 a1 = Account.new
 
-a1 # => #<Account:0x00007fccba948610 @balance=0, @name="checking">
+a1 # => #<Account:0x00007f802797f550 @balance=0, @name="checking">
 a1.deposit(15)
-a1 # => #<Account:0x00007fccba948610 @balance=15, @name="checking">
+a1 # => #<Account:0x00007f802797f550 @balance=15, @name="checking">
 
 a1.deposit(12)
-a1 # => #<Account:0x00007fccba948610 @balance=27, @name="checking">
+a1 # => #<Account:0x00007f802797f550 @balance=27, @name="checking">
 
 # 27
 a1.balance # => 27
 # ~ > -:20:in `<main>': undefined method `balance' for #<Account:0x00007fa3529b7218 @balance=27> (NoMethodError)
 
-a2 = Account.new # => #<Account:0x00007fccba91bae8 @balance=0, @name="checking">
+a1.name # => "checking"
+
+a2 = Account.new # => #<Account:0x00007f8027979dd0 @balance=0, @name="checking">
 a2.class # => Account
 
 # 0
 a2.balance # => 0
 
-a2.deposit(10) # => #<Account:0x00007fccba91bae8 @balance=10, @name="checking">
-a2.deposit(10).deposit(20) # => #<Account:0x00007fccba91bae8 @balance=40, @name="checking">
-a2 # => #<Account:0x00007fccba91bae8 @balance=40, @name="checking">
+a2.deposit(10) # => #<Account:0x00007f8027979dd0 @balance=10, @name="checking">
+a2.deposit(10).deposit(20) # => #<Account:0x00007f8027979dd0 @balance=40, @name="checking">
+a2 # => #<Account:0x00007f8027979dd0 @balance=40, @name="checking">
 
-a2.deposit(1000) # => #<Account:0x00007fccba91bae8 @balance=1040, @name="checking">
+a2.deposit(1000) # => #<Account:0x00007f8027979dd0 @balance=1040, @name="checking">
 a2.balance # => 1040
 
 # require 'pry'
 # binding.pry
 
-a3 = Account.new(1000) # => #<Account:0x00007fccba93e0c0 @balance=1000, @name="checking">
-a3 # => #<Account:0x00007fccba93e0c0 @balance=1000, @name="checking">
-a3.deposit(15) # => #<Account:0x00007fccba93e0c0 @balance=1015, @name="checking">
-a3.deposit(12) # => #<Account:0x00007fccba93e0c0 @balance=1027, @name="checking">
+a3 = Account.new(1000) # => #<Account:0x00007f8027943758 @balance=1000, @name="checking">
+a3 # => #<Account:0x00007f8027943758 @balance=1000, @name="checking">
+a3.deposit(15) # => #<Account:0x00007f8027943758 @balance=1015, @name="checking">
+a3.deposit(12) # => #<Account:0x00007f8027943758 @balance=1027, @name="checking">
 a3.balance # => 1027
 
 # add withdrawal fee of $3, balance was `1027` expect `1014` after withdraw `10`
 a3.withdraw(10) # => 10
-a3 # => #<Account:0x00007fccba93e0c0 @balance=1014, @name="checking">
+a3 # => #<Account:0x00007f8027943758 @balance=1014, @name="checking">
 
-a3.get_name # => "checking"
-a3.set_name('savings') # => "savings"
-a3.get_name # => "savings"
+# a3.get_name # => "checking"
+# a3.set_name('savings') # => "savings"
+# a3.get_name # => "savings"
+
+a3.name # => "checking"
+a3.name=('savings') # => "savings"
+a3.name = 'savings' # => "savings"
+a3.name # => "savings"
