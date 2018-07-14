d75c06bcea88aeaa52a58dc45ccc37396e24d5ec
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-14 20:07:05 +1000

    Refactor Client class into its own script
    Refactor the Happi Tails application and extract the Client class into
    its own script to emphasise the `require` command to include external
    scripts to build up a large application.

---
+++ Alex/wk04/5-fri/04_homework_happi_tials/client.rb
@@ -0,0 +1,25 @@
+# ---
+# source:
+# - title: happitials.md
+#   url: https://gist.github.com/epoch/69ce72204a35f72da045
+# ---
+
+# - Client:
+#   - A client should have a name.
+#   - A client should have a number of children.
+#   - A client should have an age.
+#   - A client should have a list of pets.
+
+class Client
+  attr_reader :name, :num_children, :age, :pets
+
+  # def initialize(name = '', age = 0, children = [], pets = [])
+  def initialize(name = '', age = 0, num_children = 0, *pets)
+    @name = name
+    @age = age
+    @num_children = num_children
+    @pets = pets
+  end
+end
+
+Client.new # => #<Client:0x00007fadc693e010 @name="", @age=0, @num_children=0, @pets=[]>
--- Alex/wk04/5-fri/04_homework_happi_tials/happitials.rb
+++ Alex/wk04/5-fri/04_homework_happi_tials/happitials.rb
@@ -7,7 +7,9 @@
 $LOAD_PATH.push File.expand_path('../', __FILE__)
 
 # require_relative 'animal'
+# require_relative 'client'
 require 'animal'
+require 'client'
 
 # # HappiTails
 
@@ -20,26 +22,6 @@ require 'animal'
 
 # ### Specification:
 # ##### Object Specs:
-
-# - Client:
-#   - A client should have a name.
-#   - A client should have a number of children.
-#   - A client should have an age.
-#   - A client should have a list of pets.
-
-class Client
-  attr_reader :name, :num_children, :age, :pets
-
-  # def initialize(name = '', age = 0, children = [], pets = [])
-  def initialize(name = '', age = 0, num_children = 0, *pets)
-    @name = name
-    @age = age
-    @num_children = num_children
-    @pets = pets
-  end
-end
-
-Client.new # => #<Client:0x00007fadc693e010 @name="", @age=0, @num_children=0, @pets=[]>
 
 # - Shelter:
 #   - The shelter should display all the clients.
