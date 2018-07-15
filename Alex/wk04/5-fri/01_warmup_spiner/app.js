3b4daa1513c026c3076138e941f85c59b6764bc6
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-15 12:46:27 +1000

    Add solution to Spiner JavaScript problem
    Add a solution to the Spiner JavaScript problem that converts spaces
    between words to dashes.

---
+++ Alex/wk04/5-fri/01_warmup_spiner/app.js
@@ -0,0 +1,24 @@
+// ---
+// source:
+// - title: WDI Spiner Warmup
+//   url: https://gist.github.com/kasun-maldeni/df34ad18c7cabfabd597fc5f6736b0b9
+// ---
+
+// # Finish Him!
+
+// In JavaScript, write a function 'spiner' that takes a string as an argument and
+// returns the string as all lowercase words joined by dashes. Your function should
+// account for upper/lower case inputs, as well as sentences __not__ seperated by
+// spaces.
+
+function spiner(sentence) {
+  return sentence.toLowerCase().replace(/[^a-z]/gi, "-");
+}
+
+// ```javascript
+spiner("All Lowercase Words Joined By Dashes") // --> "all-lowercase-words-joined-by-dashes"
+console.log(spiner("All Lowercase Words Joined By Dashes"));
+
+spiner("The_Andy_Griffith_Show") // --> "the-andy-griffith-show"
+console.log(spiner("The_Andy_Griffith_Show"));
+// ```
