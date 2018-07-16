e6051ca09cd7ba2811bac938d17916221611a0a1
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-16 10:24:16 +1000

    Add Coffee & Starbucks as separate scripts
    Add the Coffee class and Starbucks application scripts as separate
    scripts in order to demonstrate `require` within these files as part of
    the warmup exercise (for Week 5 Day 1 Warmup).

---
+++ Alex/wk05/1-mon/01_warmup_starbucks/coffee.rb
@@ -0,0 +1,28 @@
+class Coffee
+  attr_accessor :collected
+  attr_reader :customer, :name, :options, :size, :sugar, :ready_at
+
+  def initialize(customer, name, size, sugar, *options)
+    @customer = customer
+    @name = name
+    @size = size
+    @sugar = sugar
+    @options = options
+
+    wait_time = 60 * rand(2..5) # 60 seconds * 2-5 minutes
+    @ready_at = Time.now + wait_time
+  end
+
+  def ready?(time = Time.now)
+    (ready_at - time).negative?
+  end
+
+  def to_s(time = Time.now)
+    misspelling = customer.tr("Dy", "Be")
+    result = "#{misspelling}'s #{name}, #{size}, #{sugar} sugars."
+    result = result[0..-1] + " (with #{options.join(' ')})." if options.any?
+    # ready?(Time.now + 300) ? result.upcase : result
+    # ready? ? result.upcase : result
+    ready?(time) ? result.upcase : result
+  end
+end
