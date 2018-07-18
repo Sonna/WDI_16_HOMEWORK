b35852860af596409b888b6e08c28dfae674a66a
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-19 07:57:15 +1000

    Refactor index template to wrap pagination results
    Refactor an `index` template to wrap pagination around the results so
    that the results count is within the pagination template, rather than
    outside it, and place it above the listing of results.
    
    == Notes:
    - Fix URLs in `extras` to work with the database as well as the external
      API, otherwise its not found in the database and instead does a remote
      call

--- Alex/wk05/3-wed/04_movies_III_homework/main.rb
+++ Alex/wk05/3-wed/04_movies_III_homework/main.rb
@@ -45,27 +45,27 @@ get "/" do
     'page' => page,
     'apikey' => ENV['API_KEY']
   })
+
+  locals = { response: true, results: results['Search'] }
+
+  return erb(:index, locals: locals.merge({
+    response: false
+  })) if results['Response'] == 'False'
+
+  redirect "/#{URI::encode(params['movie_name'])}" if results['totalResults'] == "1"
+
   pagination = Pagination.new(
     total_items: results['totalResults'],
     items_per_page: 10,
     current_page: params['page']
-  )
+  ).to_h
 
-  locals = {
-    response: true,
-    results: results['Search'],
-    total_results: pagination.total_items,
-    result_count: pagination.result_count,
-    pagination: pagination.to_h
-  }
-
-  return erb(:index, locals: locals.merge({
-    response: false
-  })) if results['Response'] == 'False' # results['Search'].length.zero?
-
-  redirect "/#{URI::encode(params['movie_name'])}" if results['totalResults'] == "1"
-
-  erb(:index, locals: locals)
+  erb(:index, locals: locals) do
+    erb(:pagination, locals: pagination) do
+      # erb(:movies, locals: { results: locals[:results] })
+      erb(:movies, locals: locals)
+    end
+  end
 end
 
 get "/about" do
