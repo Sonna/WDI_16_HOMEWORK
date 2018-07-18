7b88d43cc32dcfbbe4ef18ff70e0c8e6f7a3513b
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-18 10:31:05 +1000

    Refactor reducing comparison or adding Dice
    Refactor the reducing of comparing or adding Dice together within
    various operations of the Roll class / value object.

--- Alex/wk05/3-wed/01_warmup_dice/roll.rb
+++ Alex/wk05/3-wed/01_warmup_dice/roll.rb
@@ -88,12 +88,8 @@ class Dice
       when self.class then pips <=> other.pips
       when Numeric then pips <=> other
       else
-        if other.respond_to? :coerce
-          a, b = other.coerce(self)
-          a + b
-        else
-          raise TypeError, "#{other.class} can't be coerced into #{self.class}"
-        end
+        return other.coerce(self).reduce(:<=>) if other.respond_to?(:coerce)
+        raise TypeError, "#{other.class} can't be coerced into #{self.class}"
       end
     end
 
@@ -104,12 +100,8 @@ class Dice
       when self.class then pips + other.pips
       when Numeric then pips + other
       else
-        if other.respond_to? :coerce
-          a, b = other.coerce(self)
-          a + b
-        else
-          raise TypeError, "#{other.class} can't be coerced into #{self.class}"
-        end
+        return other.coerce(self).reduce(:+) if other.respond_to?(:coerce)
+        raise TypeError, "#{other.class} can't be coerced into #{self.class}"
       end
     end
 
@@ -123,12 +115,12 @@ class Dice
   end
 end
 
-Dice.roll # => #<Dice::Results:0x00007fd5a90695b0 @rolls=[#<Dice::Roll:0x00007fd5a90693f8 @pips=1>]>
+Dice.roll # => #<Dice::Results:0x00007fe9cf985c90 @rolls=[#<Dice::Roll:0x00007fe9cf985bc8 @pips=3>]>
 Dice.roll.to_a # => [3]
-Dice.roll(3) # => #<Dice::Results:0x00007fd5a9059520 @rolls=[#<Dice::Roll:0x00007fd5a9059250 @pips=2>, #<Dice::Roll:0x00007fd5a9058a80 @pips=4>, #<Dice::Roll:0x00007fd5a9058a30 @pips=5>]>
-Dice.roll(3).to_a # => [1, 4, 5]
-Dice.roll(3).total # => [[6, 6, 1], 13]
-Dice.roll(3).to_s # => "_________\n| *     |\n|       |\n|     * |\n_________\n_________\n|       |\n|   *   |\n|       |\n_________\n_________\n| *   * |\n|   *   |\n| *   * |\n_________\n"
+Dice.roll(3) # => #<Dice::Results:0x00007fe9cf932fb8 @rolls=[#<Dice::Roll:0x00007fe9cf932bf8 @pips=6>, #<Dice::Roll:0x00007fe9cf932b08 @pips=5>, #<Dice::Roll:0x00007fe9cf932928 @pips=2>]>
+Dice.roll(3).to_a # => [2, 2, 5]
+Dice.roll(3).total # => [[1, 5, 2], 8]
+Dice.roll(3).to_s # => "_________\n| *     |\n|   *   |\n|     * |\n_________\n_________\n|       |\n|   *   |\n|       |\n_________\n_________\n| *     |\n|   *   |\n|     * |\n_________\n"
 puts Dice.roll(3)
 
 if $PROGRAM_NAME == __FILE__
@@ -192,25 +184,25 @@ if $PROGRAM_NAME == __FILE__
   end
 end
 # >> _________
-# >> | *   * |
-# >> | *   * |
-# >> | *   * |
-# >> _________
-# >> _________
-# >> | *     |
-# >> |   *   |
-# >> |     * |
-# >> _________
-# >> _________
 # >> |       |
 # >> |   *   |
 # >> |       |
 # >> _________
-# >> Run options: --seed 31370
+# >> _________
+# >> | *   * |
+# >> |   *   |
+# >> | *   * |
+# >> _________
+# >> _________
+# >> | *   * |
+# >> | *   * |
+# >> | *   * |
+# >> _________
+# >> Run options: --seed 26736
 # >>
 # >> # Running:
 # >>
 # >> ......
 # >>
-# >> Finished in 0.001474s, 4070.5563 runs/s, 6105.8345 assertions/s.
+# >> Finished in 0.001238s, 4846.5267 runs/s, 7269.7900 assertions/s.
 # >> 6 runs, 9 assertions, 0 failures, 0 errors, 0 skips
