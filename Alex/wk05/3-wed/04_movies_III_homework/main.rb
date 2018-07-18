23d25335984682070a31ffc6133d98220b5533f5
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-19 08:16:31 +1000

    Refactor Movie Show into its own script
    Refactor the Movie Show method into its own Action script, that accepts
    `params` as an argument and returns an expected rendered ERB template.
    
    This change helps simplify the `main.rb` script to be just be the
    Sinatra Router for adding paths to various actions within an
    application.

--- Alex/wk05/3-wed/04_movies_III_homework/main.rb
+++ Alex/wk05/3-wed/04_movies_III_homework/main.rb
@@ -10,6 +10,7 @@ require "pg"
 require "uri"
 
 require "movie_index"
+require "movie_show"
 
 require "pagination"
 require "rating"
@@ -18,87 +19,5 @@ require "sql"
 Dotenv.load File.join(File.dirname(__FILE__), ".env")
 
 get("/") { movie_index(params) }
-
-get "/about" do
-  erb :about
-end
-
-get "/:title" do
-  title = params['title']
-  # Find movie (by title)
-  sql = "SELECT * FROM movies WHERE title ILIKE ($1);"
-  result = prepare_sql("find_movie", sql, title).first
-
-  # if found
-  if result
-    # - store in result
-    result["response"] = "True"
-    result.transform_keys!(&:to_sym)
-  # if no result
-  else
-    # - query remote API
-    # - store in result
-    result = HTTParty.get(ENV['API_URL'], query: {
-      "t" => title,
-      "apikey" => ENV['API_KEY']
-    })
-
-    # - save result to database
-    unless result["Error"]
-      sql = <<~SQL
-        INSERT INTO movies (
-          Title, Year, Rated,
-          Released, Runtime, Genre,
-          Director, Writer, Actors,
-          Plot, Language, Poster,
-          imdbRating, imdbVotes,
-          Production
-        ) VALUES (
-          $1, $2, $3, $4,
-          $5, $6, $7,
-          $8, $9, $10, $11,
-          $12, $13, $14,
-          $15
-        );
-      SQL
-
-      prepare_sql(
-        "create_movie", sql,
-        result["Title"], result["Year"], result["Rated"], result["Released"],
-        result["Runtime"], result["Genre"], result["Director"],
-        result["Writer"], result["Actors"], result["Plot"], result["Language"],
-        result["Poster"], result["imdbRating"], result["imdbVotes"],
-        result["Production"]
-      )
-    end
-
-    result = result.transform_keys { |key| key.downcase.to_sym }
-  end
-
-  # render
-  erb :movie, locals: {
-    response: result[:response] == "True",
-    param_title: title,
-
-    title: result[:title],
-    year: result[:year],
-    rated: result[:rated],
-    released: result[:released],
-    runtime: result[:runtime],
-    genre: result[:genre],
-    director: result[:director],
-    writer: result[:writer],
-    actors: result[:actors],
-    plot: result[:plot],
-    language: result[:language],
-    poster_url: result[:poster],
-    imdb_rating: Rating.new(result[:imdbrating].to_f),
-    imdb_votes: result[:imdbvotes],
-    production: result[:production],
-
-    result: result,
-    # result: result.parsed_response,
-    error_message: result[:error]
-    # error_message: result.parsed_response
-  }
-end
+get("/about") { erb :about }
+get("/:title") { movie_show }
