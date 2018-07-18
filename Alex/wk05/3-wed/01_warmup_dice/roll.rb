27b0627f4328cd8af6dbf6d5fae7a56849657c4c
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-18 10:36:55 +1000

    Refactor common operators into define_method
    Refactor the common operators into a `define_method` block that sends
    the operator to the first argument with the other value and substitutes
    in the operator, since the body of work is the same for each operator
    expect for the operator being used, hence the refactoring.
    
    This will also allow for easier access to add more operators should they
    be needed.
    
    == References:
    - https://www.mutuallyhuman.com/blog/2011/01/25/class-coercion-in-ruby
    - https://stackoverflow.com/questions/2799571/in-ruby-how-does-coerce-actually-work
    - https://ruby-doc.org/core-2.2.0/Module.html#method-i-define_method

--- Alex/wk05/3-wed/01_warmup_dice/roll.rb
+++ Alex/wk05/3-wed/01_warmup_dice/roll.rb
@@ -83,25 +83,15 @@ class Dice
       @pips = pips
     end
 
-    def <=>(other)
-      case other
-      when self.class then pips <=> other.pips
-      when Numeric then pips <=> other
-      else
-        return other.coerce(self).reduce(:<=>) if other.respond_to?(:coerce)
-        raise TypeError, "#{other.class} can't be coerced into #{self.class}"
-      end
-    end
-
-    def +(other)
-      case other
-      # Does this idea of adding pips, to make a new Die/Roll, make sense?
-      # self.class.new(pips + other.pips)
-      when self.class then pips + other.pips
-      when Numeric then pips + other
-      else
-        return other.coerce(self).reduce(:+) if other.respond_to?(:coerce)
-        raise TypeError, "#{other.class} can't be coerced into #{self.class}"
+    [:+, :<=>].each do |optr|
+      define_method(optr) do |other|
+        case other
+        when self.class then pips.send(optr, other.pips)
+        when Numeric then pips.send(optr, other)
+        else
+          return other.coerce(self).reduce(optr) if other.respond_to?(:coerce)
+          raise TypeError, "#{other.class} can't be coerced into #{self.class}"
+        end
       end
     end
 
@@ -115,12 +105,12 @@ class Dice
   end
 end
 
-Dice.roll # => #<Dice::Results:0x00007fe9cf985c90 @rolls=[#<Dice::Roll:0x00007fe9cf985bc8 @pips=3>]>
-Dice.roll.to_a # => [3]
-Dice.roll(3) # => #<Dice::Results:0x00007fe9cf932fb8 @rolls=[#<Dice::Roll:0x00007fe9cf932bf8 @pips=6>, #<Dice::Roll:0x00007fe9cf932b08 @pips=5>, #<Dice::Roll:0x00007fe9cf932928 @pips=2>]>
-Dice.roll(3).to_a # => [2, 2, 5]
-Dice.roll(3).total # => [[1, 5, 2], 8]
-Dice.roll(3).to_s # => "_________\n| *     |\n|   *   |\n|     * |\n_________\n_________\n|       |\n|   *   |\n|       |\n_________\n_________\n| *     |\n|   *   |\n|     * |\n_________\n"
+Dice.roll # => #<Dice::Results:0x00007fd73f986638 @rolls=[#<Dice::Roll:0x00007fd73f9865c0 @pips=1>]>
+Dice.roll.to_a # => [5]
+Dice.roll(3) # => #<Dice::Results:0x00007fd73f984130 @rolls=[#<Dice::Roll:0x00007fd73f933f28 @pips=6>, #<Dice::Roll:0x00007fd73f933d48 @pips=2>, #<Dice::Roll:0x00007fd73f933c58 @pips=2>]>
+Dice.roll(3).to_a # => [5, 6, 5]
+Dice.roll(3).total # => [[6, 1, 4], 11]
+Dice.roll(3).to_s # => "_________\n| *   * |\n|   *   |\n| *   * |\n_________\n_________\n| *     |\n|   *   |\n|     * |\n_________\n_________\n|       |\n|   *   |\n|       |\n_________\n"
 puts Dice.roll(3)
 
 if $PROGRAM_NAME == __FILE__
@@ -184,25 +174,25 @@ if $PROGRAM_NAME == __FILE__
   end
 end
 # >> _________
+# >> | *     |
+# >> |       |
+# >> |     * |
+# >> _________
+# >> _________
 # >> |       |
 # >> |   *   |
 # >> |       |
 # >> _________
 # >> _________
-# >> | *   * |
+# >> | *     |
 # >> |   *   |
-# >> | *   * |
+# >> |     * |
 # >> _________
-# >> _________
-# >> | *   * |
-# >> | *   * |
-# >> | *   * |
-# >> _________
-# >> Run options: --seed 26736
+# >> Run options: --seed 34449
 # >>
 # >> # Running:
 # >>
 # >> ......
 # >>
-# >> Finished in 0.001238s, 4846.5267 runs/s, 7269.7900 assertions/s.
+# >> Finished in 0.001324s, 4531.7221 runs/s, 6797.5831 assertions/s.
 # >> 6 runs, 9 assertions, 0 failures, 0 errors, 0 skips
