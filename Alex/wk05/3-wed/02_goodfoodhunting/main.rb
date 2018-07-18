b20769ee5a1d02931db31a67aabf699412e08125
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-18 11:16:39 +1000

    Remove unnecessary duplication & comments
    Remove unnecessary duplication and comments within Good Food Hunting
    that were part of the working documents in the previous day, but are not
    necessary, but just noise in todays work.

--- Alex/wk05/3-wed/02_goodfoodhunting/main.rb
+++ Alex/wk05/3-wed/02_goodfoodhunting/main.rb
@@ -5,29 +5,20 @@ require 'sinatra/reloader'
 
 get '/' do
   conn = PG.connect(dbname: "goodfoodhunting", port: 5433, user: "postgres", hostaddr: "::")
-  # @dishes = []
   @dishes = conn.exec("SELECT * FROM dishes;")
   conn.close
   erb :index
 end
 
-get '/donate' do
-  return 'me me me'
+get '/about' do
 end
 
-# get '/:dishname' do
 get '/dishes/:dishname' do
   conn = PG.connect(dbname: "goodfoodhunting", port: 5433, user: "postgres", hostaddr: "::")
   sql = "SELECT * FROM dishes WHERE name = '#{ params[:dishname] }';"
   result = conn.exec(sql)
   @dish = result.first
   erb :dish_details, locals: { name: @dish["name"], image_url: @dish["image_url"] }
-  # erb :dish, locals: { name: @dish["name"], image_url: @dish["image_url"] }
 ensure
   conn.close
 end
-
-# post "/" do
-#   params['name']
-#   params['image_url']
-# end
