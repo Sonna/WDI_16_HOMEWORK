cb6aeda4d1a9b399135640fbe13cf7b672f6c6a4
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-19 23:54:40 +1000

    Add Movie Show Action class
    Add the Movie Show Action class to wrap up the Movie show controller
    action within a class to make it easier to process the given parameters
    for a given route, whilst allowing for easier re-routing or multiple
    routes that render the displayed movie.

--- Alex/wk05/3-wed/04_movies_III_homework/actions/movie_show.rb
+++ Alex/wk05/3-wed/04_movies_III_homework/actions/movie_show.rb
@@ -1,79 +1,91 @@
-def movie_show(params)
-  title = params['title']
-  # Find movie (by title)
-  sql = "SELECT * FROM movies WHERE title ILIKE ($1);"
-  result = prepare_sql("find_movie", sql, title).first
+class MovieShowAction
+  include ErbRender
 
-  # if found
-  if result
-    # - store in result
-    result["response"] = "True"
-    result.transform_keys!(&:to_sym)
-  # if no result
-  else
-    # - query remote API
-    # - store in result
-    result = HTTParty.get(ENV['API_URL'], query: {
-      "t" => title,
-      "apikey" => ENV['API_KEY']
-    })
+  attr_reader :params
 
-    # - save result to database
-    unless result["Error"]
-      sql = <<~SQL
-        INSERT INTO movies (
-          Title, Year, Rated,
-          Released, Runtime, Genre,
-          Director, Writer, Actors,
-          Plot, Language, Poster,
-          imdbRating, imdbVotes,
-          Production
-        ) VALUES (
-          $1, $2, $3, $4,
-          $5, $6, $7,
-          $8, $9, $10, $11,
-          $12, $13, $14,
-          $15
-        );
-      SQL
-
-      prepare_sql(
-        "create_movie", sql,
-        result["Title"], result["Year"], result["Rated"], result["Released"],
-        result["Runtime"], result["Genre"], result["Director"],
-        result["Writer"], result["Actors"], result["Plot"], result["Language"],
-        result["Poster"], result["imdbRating"], result["imdbVotes"],
-        result["Production"]
-      )
-    end
-
-    result = result.transform_keys { |key| key.downcase.to_sym }
+  def initialize(params)
+    @params = params
   end
 
-  # render
-  erb :movie, locals: {
-    response: result[:response] == "True",
-    param_title: title,
+  def to_template
+    title = params['title']
+    # Find movie (by title)
+    sql = "SELECT * FROM movies WHERE title ILIKE ($1);"
+    result = prepare_sql("find_movie", sql, title).first
 
-    title: result[:title],
-    year: result[:year],
-    rated: result[:rated],
-    released: result[:released],
-    runtime: result[:runtime],
-    genre: result[:genre],
-    director: result[:director],
-    writer: result[:writer],
-    actors: result[:actors],
-    plot: result[:plot],
-    language: result[:language],
-    poster_url: result[:poster],
-    imdb_rating: Rating.new(result[:imdbrating].to_f),
-    imdb_votes: result[:imdbvotes],
-    production: result[:production],
+    # if found
+    if result
+      # - store in result
+      result["response"] = "True"
+      result.transform_keys!(&:to_sym)
+    # if no result
+    else
+      # - query remote API
+      # - store in result
+      result = HTTParty.get(ENV['API_URL'], query: {
+        "t" => title,
+        "apikey" => ENV['API_KEY']
+      })
 
-    result: result,
-    # result: result.parsed_response,
-    error_message: result[:error]
-    # error_message: result.parsed_response
-  }
+      # - save result to database
+      unless result["Error"]
+        sql = <<~SQL
+          INSERT INTO movies (
+            Title, Year, Rated,
+            Released, Runtime, Genre,
+            Director, Writer, Actors,
+            Plot, Language, Poster,
+            imdbRating, imdbVotes,
+            Production
+          ) VALUES (
+            $1, $2, $3, $4,
+            $5, $6, $7,
+            $8, $9, $10, $11,
+            $12, $13, $14,
+            $15
+          );
+        SQL
+
+        prepare_sql(
+          "create_movie", sql,
+          result["Title"], result["Year"], result["Rated"], result["Released"],
+          result["Runtime"], result["Genre"], result["Director"],
+          result["Writer"], result["Actors"], result["Plot"], result["Language"],
+          result["Poster"], result["imdbRating"], result["imdbVotes"],
+          result["Production"]
+        )
+      end
+
+      result = result.transform_keys { |key| key.downcase.to_sym }
+    end
+
+    # render
+    erb :movie, layout: :layout, locals: {
+      response: result[:response] == "True",
+      param_title: title,
+
+      title: result[:title],
+      year: result[:year],
+      rated: result[:rated],
+      released: result[:released],
+      runtime: result[:runtime],
+      genre: result[:genre],
+      director: result[:director],
+      writer: result[:writer],
+      actors: result[:actors],
+      plot: result[:plot],
+      language: result[:language],
+      poster_url: result[:poster],
+      imdb_rating: Rating.new(result[:imdbrating].to_f),
+      imdb_votes: result[:imdbvotes],
+      production: result[:production],
+
+      result: result,
+      error_message: result[:error]
+    }
+  end
+end
+
+def movie_show(params)
+  MovieShowAction.new(params).to_template
 end
