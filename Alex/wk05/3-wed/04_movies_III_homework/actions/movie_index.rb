f883a6075c6d4ecef50a61e10a29690e1f498842
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-19 23:47:49 +1000

    Refactor variables within Movie III app index
    Refactor the various variables within the Movie 3 application Index
    Action, so that be more easily referenced or re-referenced within the
    body of work to render the template. Thus leaving the only
    responsibilities within `to_template` to be about returning a template
    on various conditions and not setting up variables.`

--- Alex/wk05/3-wed/04_movies_III_homework/actions/movie_index.rb
+++ Alex/wk05/3-wed/04_movies_III_homework/actions/movie_index.rb
@@ -9,33 +9,41 @@ class MovieIndexAction
     @params = params
   end
 
-  def to_template
-    return erb(:index, layout: :layout, locals: { response: nil, results: [] }) if params['movie_name'].nil?
+  def locals
+    { response: true, results: results["Search"], error_message: results["Error"] }
+  end
 
-    page = params['page'] ? params['page'].to_i : 1
+  def page
+    params["page"] ? params["page"].to_i : 1
+  end
 
-    results = HTTParty.get(ENV['API_URL'], query: {
-      's' => params['movie_name'],
-      'type' => 'movie',
-      'page' => page,
-      'apikey' => ENV['API_KEY']
-    })
-
-    locals = { response: true, results: results['Search'], error_message: results["Error"] }
-
-    return erb(:index, layout: :layout, locals: locals.merge({ response: false })) if results['Response'] == 'False'
-
-    redirect "/#{URI::encode(params['movie_name'])}" if results['totalResults'] == "1"
-
-    pagination = Pagination.new(
-      total_items: results['totalResults'],
+  def pagination
+    Pagination.new(
+      total_items: results["totalResults"],
       items_per_page: 10,
-      current_page: params['page']
+      current_page: params["page"]
     ).to_h
+  end
+
+  def results
+    @results ||= {}
+    @results[[params["movie_name"], page]] ||= HTTParty.get(
+      ENV["API_URL"], query: {
+        "s" => params["movie_name"],
+        "type" => "movie",
+        "page" => page,
+        "apikey" => ENV["API_KEY"]
+      }
+    )
+  end
+
+  def to_template
+    return erb(:index, layout: :layout, locals: { response: nil, results: [] }) if params["movie_name"].nil?
+    return erb(:index, layout: :layout, locals: locals.merge({ response: false })) if results["Response"] == "False"
+    redirect %(/#{URI::encode(params["movie_name"])}) if results["totalResults"] == "1"
 
     erb(:index, layout: :layout, locals: locals) do
       erb(:pagination, locals: pagination) do
-        # erb(:movies, locals: { results: locals[:results] })
         erb(:movies, locals: locals)
       end
     end
