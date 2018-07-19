58585da2c0fdc37fbabfa0c8deba241047681112
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-19 11:51:49 +1000

    Add commented example of POINTS hash generation
    Add a commented code example for generating the POINTS Hash for scoring
    Letters in a Game of Scrabble.

--- Alex/wk05/4-thu/01_warmup_scrabble/scrabble.rb
+++ Alex/wk05/4-thu/01_warmup_scrabble/scrabble.rb
@@ -33,16 +33,30 @@ module Scrabble
     ->(l) { [l, v] }
   end
 
+  POINTS = ('A'..'Z').to_a.zip(
+    [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10]
+  ).to_h.freeze
+
   # Letter                           Value
-  POINTS = Hash[*[
-    %w(A E I O U L N R S T).map(&letter_value(1)),
-    %w(D G).map(&letter_value(2)),
-    %w(B C M P).map(&letter_value(3)),
-    %w(F H V W Y).map(&letter_value(4)),
-    %w(K).map(&letter_value(5)),
-    %w(J X).map(&letter_value(8)),
-    %w(Q Z).map(&letter_value(10))
-  ].flatten].freeze
+  # POINTS = Hash[*[
+  #   %w(A E I O U L N R S T).map(&letter_value(1)),
+  #   %w(D G).map(&letter_value(2)),
+  #   %w(B C M P).map(&letter_value(3)),
+  #   %w(F H V W Y).map(&letter_value(4)),
+  #   %w(K).map(&letter_value(5)),
+  #   %w(J X).map(&letter_value(8)),
+  #   %w(Q Z).map(&letter_value(10))
+  # ].flatten].freeze
+
+  # POINTS = {
+  #   A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  #   D: 2, G: 2,
+  #   B: 3, C: 3, M: 3, P: 3,
+  #   F: 4, H: 4, V: 4, W: 4, Y: 4,
+  #   K: 5,
+  #   J: 8, X: 8,
+  #   Q: 10, Z: 10
+  # }.freeze
 
   MODIFIERS = { nil => 1, double: 2, triple: 3 }.freeze
 
@@ -82,41 +96,41 @@ if $PROGRAM_NAME == __FILE__
 
     def test_calculates_score_1_for_AEIOULNRST
       %w(a e i o u l n r s t).each do |letter|
-        assert_equal 1, described_class.score(letter)
+        assert_equal 1, described_class.score(letter), letter
       end
     end
 
     def test_calculates_score_2_for_DG
       %w(d g).each do |letter|
-        assert_equal 2, described_class.score(letter)
+        assert_equal 2, described_class.score(letter), letter
       end
     end
 
     def test_calculates_score_3_for_BCMP
       %w(b c m p).each do |letter|
-        assert_equal 3, described_class.score(letter)
+        assert_equal 3, described_class.score(letter), letter
       end
     end
 
     def test_calculates_score_4_for_FHVWY
       %w(f h v w y).each do |letter|
-        assert_equal 4, described_class.score(letter)
+        assert_equal 4, described_class.score(letter), letter
       end
     end
 
     def test_calculates_score_5_for_K
-      assert_equal 5, described_class.score("k")
+      assert_equal 5, described_class.score("k"), "k"
     end
 
     def test_calculates_score_8_for_JX
       %w(j x).each do |letter|
-        assert_equal 8, described_class.score(letter)
+        assert_equal 8, described_class.score(letter), letter
       end
     end
 
     def test_calculates_score_10_for_QZ
       %w(q z).each do |letter|
-        assert_equal 10, described_class.score(letter)
+        assert_equal 10, described_class.score(letter), letter
       end
     end
 
@@ -151,11 +165,11 @@ if $PROGRAM_NAME == __FILE__
     end
   end
 end
-# >> Run options: --seed 64495
+# >> Run options: --seed 17885
 # >>
 # >> # Running:
 # >>
 # >> ................
 # >>
-# >> Finished in 0.002996s, 5340.4539 runs/s, 11682.2430 assertions/s.
+# >> Finished in 0.001868s, 8565.3105 runs/s, 18736.6168 assertions/s.
 # >> 16 runs, 35 assertions, 0 failures, 0 errors, 0 skips
