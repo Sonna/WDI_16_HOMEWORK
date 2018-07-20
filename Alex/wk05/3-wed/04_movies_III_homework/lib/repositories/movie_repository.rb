76a9612a21baf5a18db7312b61797d0dc0b32f53
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-20 18:51:48 +1000

    Refactor common SQL methods within Repository
    Refactor common SQL methods for building SQL queries and connecting to
    the database within a Base Repository object to make it easier to
    potentially reuse these find by or create methods should the application
    be expanded out to use other models or be completely refactored into a
    gem.

--- Alex/wk05/3-wed/04_movies_III_homework/lib/repositories/movie_repository.rb
+++ Alex/wk05/3-wed/04_movies_III_homework/lib/repositories/movie_repository.rb
@@ -1,42 +1,18 @@
-class MovieRepository
+class MovieRepository < BaseRepository
   ATTRIBUTES = %w(
     title year rated released runtime genre director writer actors plot language
     poster imdbrating imdbvotes production
   ).freeze
 
-  attr_reader :adapter
-
-  class << self
-    def create_sql(table = "movies", attributes = ATTRIBUTES)
-      <<~SQL
-        INSERT INTO #{ table } (
-          #{ attributes.join(',') }
-        ) VALUES (
-          #{ attributes.map.with_index(1) { |_, i| "$#{i}" }.join(",") }
-        );
-      SQL
-    end
-
-    def find_sql(table = "movies", attribute = "title")
-      "SELECT * FROM #{table} WHERE #{attribute} ILIKE ($1);"
-    end
-  end
-
-  def initialize(adapter = PSQL)
-    @adapter = adapter
-    # adapter.prepare("find_movie", self.class.find_sql)
-    # adapter.prepare("create_movie", self.class.create_sql)
-  end
-
-  def attributes
-    ATTRIBUTES
+  def initialize(table: "movies", adapter: PSQL, attributes: ATTRIBUTES)
+    super(table: table, adapter: adapter, attributes: attributes)
   end
 
   def create(attributes)
-    adapter.exec_prepared("create_movie", self.class.create_sql, *attributes)
+    adapter.exec_prepared("create_movie", create_sql, *attributes)
   end
 
   def find(title)
-    adapter.exec_prepared("find_movie", self.class.find_sql, title).first
+    adapter.exec_prepared("find_movie", find_sql("title"), title).first
   end
 end
