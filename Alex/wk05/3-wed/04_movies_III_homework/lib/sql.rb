a9252a6454ee9816b2e82f0ef624c82ab89c76c6
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-20 18:59:11 +1000

    Remove unused SQL methods, since PSQL usurps them
    Remove the unused SQL methods since the PSQL module usurps them
    entirely, despite them being only used in one other class.

--- Alex/wk05/3-wed/04_movies_III_homework/lib/sql.rb
+++ Alex/wk05/3-wed/04_movies_III_homework/lib/sql.rb
@@ -1,22 +1,3 @@
-def conn_sql
-  conn = PG.connect(dbname: ENV["DB_NAME"], port: ENV["DB_PORT"],
-                    user: ENV["DB_USER"], hostaddr: ENV["DB_ADDR"])
-  yield conn
-ensure
-  conn.close
-end
-
-def run_sql(sql)
-  conn_sql { |conn| conn.exec(sql) }
-end
-
-def prepare_sql(name, sql, *args)
-  conn_sql do |conn|
-    conn.prepare(name, sql)
-    conn.exec_prepared(name, args)
-  end
-end
-
 module PSQL
   class << self
     def connect
