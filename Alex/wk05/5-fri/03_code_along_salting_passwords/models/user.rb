2809759bc28636ee0ec39bc3e6a8750600fddab0
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-20 12:57:37 +1000

    Add validations to User model signup route
    Add validations to User model signup route that returns the User back to
    the login page, should it fail validation.

--- Alex/wk05/5-fri/03_code_along_salting_passwords/models/user.rb
+++ Alex/wk05/5-fri/03_code_along_salting_passwords/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   # add a method - password
   # add another method - authentication
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
