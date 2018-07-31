595e3c14a9981cc9ac9e817600042bc7a1bb57b5
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-08-01 07:58:32 +1000

    Refactor AJAX Movie search
    Refactor the AJAX Movie search JavaScript into separate composable
    functions.

--- Alex/wk07/2-tue/02_ajax_movies/app.js
+++ Alex/wk07/2-tue/02_ajax_movies/app.js
@@ -1,32 +1,39 @@
-$(document).ready(function() {
+function omdbOptions(query) {
+  return {
+    method: 'GET',
+    url: 'https://omdbapi.com/?s=' + query + '&apikey=2f6435d9',
+    dataType: 'json',
+  };
+}
 
+function buildResult(result) {
+  var el = document.createElement("p");
+  el.innerHTML = '<a href="https://www.imdb.com/title/' + result["imdbID"] + '" target="_blank">' +
+    result["Title"] +
+    '</a>';
+  return el;
+}
+
+function onSuccess(data) {
+  // console.log(data);
+  if (data["Response"] === "True") {
+    var resultsEl = document.getElementById("search-results");
+    var newResultsEl = document.createElement("section");
+    newResultsEl.id = "search-results";
+
+    data["Search"].forEach(function(result) {
+      newResultsEl.appendChild(buildResult(result));
+    });
+
+    resultsEl.parentNode.replaceChild(newResultsEl, resultsEl);
+  }
+}
+
+$(document).ready(function() {
   $('#movie-search').on('submit', function(event) {
     event.preventDefault();
 
     var query = $("#movie-search input[name='query']").val();
-
-    $.ajax({
-      method: 'GET',
-      url: 'https://omdbapi.com/?s=' + query + '&apikey=2f6435d9',
-      dataType: 'json',
-    }).done(function onSuccess(data) {
-      // console.log(data);
-      if (data["Response"] === "True") {
-        var resultsEl = document.getElementById("search-results");
-        var newResultsEl = document.createElement("section");
-        newResultsEl.id = "search-results";
-
-        data["Search"].forEach(function(result) {
-          var el = document.createElement("p");
-          // el.innerText = result["Title"];
-          el.innerHTML = '<a href="https://www.imdb.com/title/' + result["imdbID"] + '" target="_blank">' +
-            result["Title"] +
-            '</a>';
-          newResultsEl.appendChild(el);
-        });
-
-        resultsEl.parentNode.replaceChild(newResultsEl, resultsEl);
-      }
-    });
+    $.ajax(omdbOptions(query)).done(onSuccess);
   });
 });
